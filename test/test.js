f7dc23192e85c7d71dc104365535c57239dfd7da
yuichi-tanaka <A12473@pc-2028.cyberagent.co.jp>
2013-11-12 19:13:30 +0900

    lotteryPerUserコミット

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var lpu = require('../lib/lotteryPerUser');
+var testUser = [1,2,3,4,5];
+var testLots = [
+  {id:1,name:'red',weight:20},
+  {id:2,name:'blue',weight:30},
+  {id:3,name:'green',weight:30},
+  {id:4,name:'yellow',weight:10},
+  {id:5,name:'black',weight:5},
+  {id:6,name:'red',weight:5}
+
+];
+var lpuins = new lpu(testUser,testLots);
+var obj = {};
+lpuins.shuffle();
+for(var i = 0 ; i < 1000; i ++){
+  var d = lpuins.drawLots(1);
+  if(!obj[d.id]) obj[d.id] = 0;
+  obj[d.id] += 1;
+}
+console.dir(obj);
